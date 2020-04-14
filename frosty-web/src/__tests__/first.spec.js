39fbf3e650e5efa60bb34ab5dcd1838635b368e9
Casey Flynn <cflynn.us@gmail.com>
2020-04-14 16:51:50 +0800

    Add some basic react-testing-library tests

--- frosty-web/src/__tests__/first.spec.js
+++ frosty-web/src/__tests__/first.spec.js
@@ -1,16 +1,66 @@
 import React from 'react'
+
+import * as Reducer from '../reducers'
+import Basic from '../components/Basic.js'
+import TodoListsMenu from '../components/TodoListsMenu.js'
+
 import {
   render,
+  fireEvent,
   cleanup
 } from '@testing-library/react'
 
-// import userEvent from '@testing-library/user-event'
-
-import App from '../components/App.js'
-
 describe('default initial state', () => {
   afterEach(cleanup)
-  it('user can view all todo lists and active todo list items', () => {
-    console.log('foo')
+  it('shows counter and clicking button increments counter', () => {
+    const {
+      getByText
+    } = render(<Basic />)
+    expect(getByText(/0/).textContent).toBe('0')
+    fireEvent.click(getByText(/Increment/i))
+    expect(getByText(/1/).textContent).toBe('1')
+  })
+
+  it('test of initial state of reducer', () => {
+    expect(Reducer.initialState).toEqual({
+      user: {
+        id: 1,
+        name: 'Casey',
+        email: 'cflynn.us@gmail.com'
+      },
+      todoLists: [
+        {
+          id: 1,
+          name: 'House Chores 1',
+          todos: [
+            {
+              id: 1,
+              value: 'Mow Lawn',
+              completed: false
+            }, {
+              id: 2,
+              value: 'Clean Dishes',
+              completed: false
+            }
+          ]
+        },
+        {
+          id: 2,
+          name: 'Work',
+          todos: [
+            {
+              id: 1,
+              value: 'Call Accountant',
+              completed: false
+            }, {
+              id: 2,
+              value: 'File Taxes',
+              completed: false
+            }
+          ]
+        }
+
+      ]
+    })
   })
 })
---
+++ frosty-web/src/components/Basic.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+export default function Basic () {
+  const [count, setCount] = useState(0)
+
+  const increment = () => {
+    setCount(count + 1)
+  }
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={increment}>Increment</button>
+    </div>
+  )
+}
